c70997528c2a041d4c131be375d994d4a9e8ad34
Jacob Logas <logasja@outlook.com>
2022-06-23 10:57:03 -0400

    Debug printing for huggingface tmp directory exploration

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from fawkes.protection import Fawkes
 import gradio as gr
+import os
 
 def predict(level, img):
   print(img)
   fwks = Fawkes("extractor_2", '0', 1, mode=level)
   fwks.run_protection([img], format='jpeg')
   splt = img.split(".")
+  print(os.listdir('/tmp'))
   return splt[0] + "_cloaked." + splt[1]
 
 gr.Interface(fn=predict, inputs=[gr.inputs.Dropdown(["low", "mid", "high"], label="Protection Level"), gr.inputs.Image(type='filepath')], outputs=gr.outputs.Image(type="pil")).launch(share=True)
